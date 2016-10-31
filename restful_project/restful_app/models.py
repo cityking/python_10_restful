ffe0cbd1d55be221607873f720f571611d4fe7ce
root <root@localhost.localdomain>
2016-11-01 00:02:41 +0800

    环境搭建

---
+++ restful_project/restful_app/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Poem(models.Model):
+	title = models.CharField(max_length=100)
+	author = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.title
